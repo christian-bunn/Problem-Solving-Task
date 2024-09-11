4113fe3ba04e2cf4dfdc7aae635e3bbf0ac13f24
Christian Bunn <ctbunn333@gmail.com>
2024-09-11 20:29:44 +1000

    Initial commit

---
+++ dbl_vector.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dbl_vector.h"
+
+void dv_init( dbl_vector_t* vec ) {
+    // INSERT SOLUTION HERE
+    vec->capacity == DV_INITIAL_CAPACITY,
+    vec->size == 0,
+    vec->data == (double*)malloc(vec->capacity * sizeof(double));
+    if (!vec->data) {
+        fprintf(stderr, "Logging a failed message for this function\n");
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+void dv_ensure_capacity( dbl_vector_t* vec, size_t new_size ) {
+    // INSERT SOLUTION HERE
+}
+
+void dv_destroy( dbl_vector_t* vec ) {
+    // INSERT SOLUTION HERE
+}
+
+void dv_copy( dbl_vector_t* vec, dbl_vector_t* dest ) {
+    // INSERT SOLUTION HERE
+}
+
+void dv_clear( dbl_vector_t* vec ) {
+    // INSERT SOLUTION HERE
+}
+
+void dv_push( dbl_vector_t* vec, double new_item ) {
+    // INSERT SOLUTION HERE
+}
+
+void dv_pop( dbl_vector_t* vec ) {
+    // INSERT SOLUTION HERE
+}
+
+double dv_last( dbl_vector_t* vec ) {
+    double result = NAN;
+    // INSERT SOLUTION HERE
+    return result;
+}
+
+void dv_insert_at( dbl_vector_t* vec, size_t pos, double new_item ) {
+    // INSERT SOLUTION HERE
+}
+
+void dv_remove_at( dbl_vector_t* vec, size_t pos ) {
+    // INSERT SOLUTION HERE
+}
+
+void dv_foreach( dbl_vector_t* vec, void (*callback)(double, void*), void* info ) {
+    // INSERT SOLUTION HERE
+}
