48afdffd4bed9578966039608aff5a165058a2ad
Christian Bunn <ctbunn333@gmail.com>
2024-09-11 20:51:11 +1000

    fixed == syntax to = and added endure and destroy functions

--- dbl_vector.c
+++ dbl_vector.c
@@ -4,9 +4,9 @@
 
 void dv_init( dbl_vector_t* vec ) {
     // INSERT SOLUTION HERE
-    vec->capacity == DV_INITIAL_CAPACITY,
-    vec->size == 0,
-    vec->data == (double*)malloc(vec->capacity * sizeof(double));
+    vec->capacity = DV_INITIAL_CAPACITY,
+    vec->size = 0,
+    vec->data = (double*)malloc(vec->capacity * sizeof(double));
     if (!vec->data) {
         fprintf(stderr, "Logging a failed message for this function\n");
         exit(EXIT_FAILURE);
@@ -16,10 +16,26 @@ void dv_init( dbl_vector_t* vec ) {
 
 void dv_ensure_capacity( dbl_vector_t* vec, size_t new_size ) {
     // INSERT SOLUTION HERE
+    if (new_size > vec->capacity) {
+        size_t new_capacity = vec->capacity * DV_GROWTH_FACTOR;
+        if (new_size > new_capacity) {
+            new_capacity = new_size;
+        }
+        vec->data = (double*)realloc(vec->data, new_capacity * sizeof(double));
+        if (!vec->data) {
+            fprintf(stderr, "Memory reallocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+        vec->capacity = new_capacity;
+    }
 }
 
 void dv_destroy( dbl_vector_t* vec ) {
     // INSERT SOLUTION HERE
+    free(vec->data);
+    vec->data = NULL;
+    vec->size = 0;
+    vec->capacity = 0;
 }
 
 void dv_copy( dbl_vector_t* vec, dbl_vector_t* dest ) {
